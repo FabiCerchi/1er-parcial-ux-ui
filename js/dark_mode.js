e01b596ace2075348d0a2a9be003d6bc1275cf0d
angelfcerchi <Sorete2340>
2023-05-12 23:01:38 -0300

    Feat: add register and dm register

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const darkModeToggle = document.getElementById("darkModeToggle");
     const darkModeEnabled = localStorage.getItem("darkModeEnabled");
-
     if (darkModeEnabled === "true") {
         enableDarkMode();
         darkModeToggle.checked = true;
@@ -18,26 +17,28 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     function enableDarkMode() {
-        /* BODY */
+        var currentPage = window.location.pathname;
+        console.log(currentPage)
+        if (currentPage == '/index.html') {
+            document.getElementById("organization").classList.add("section-main-dark-mode");
+            document.getElementById("partners").classList.add("section-main-dark-mode");
+            document.getElementById("work_envolved").classList.add("section-main-dark-mode");
+            
+        }
+        if (currentPage == '/register.html'){   
+            console.log('ingrese')
+            document.getElementById("main_register").classList.add("section-main-dark-mode");
+        }
+        document.getElementById("hero_metaverso").classList.add("main-img-metaverso-dm");
         document.body.classList.add("body-dark-mode");
-
-        /* NAV  BACKGROUND */
         document.getElementById("nav_bar").classList.add("bg-dark", "navbar-dark");
         document.getElementById("nav_bar").classList.remove("bg-body-tertiary");
-        /* NAV LOGO */
         document.getElementById("nav_logo_light").classList.add("d-none")
         document.getElementById("nav_logo_dark").classList.remove("d-none")
-        /* NAV FONT COLOR */
         document.getElementById("name_toggle").classList.add("toggleDark");
 
-
-        /* MAIN DARK MODE BG */
-        document.getElementById("work_envolved").classList.add("section-main-dark-mode");
-        document.getElementById("organization").classList.add("section-main-dark-mode");
-        document.getElementById("partners").classList.add("section-main-dark-mode");
-        /* MAIN IMG METAVERSO */
-        document.getElementById("hero_metaverso").classList.add("main-img-metaverso-dm");
         svgs = document.getElementsByClassName("svg")
+        console.log(svgs)
         Array.from(svgs).forEach(function (svg) {
             svg.classList.add("waves-dm");
         });
@@ -47,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
         Array.from(partners_light_logo).forEach(function (logo) {
             logo.classList.add("d-none")
         });
-    
+
         partners_dark_logo = document.getElementsByClassName("partner-dark-logo")
         console.log(partners_dark_logo)
         Array.from(partners_dark_logo).forEach(function (logo) {
@@ -56,26 +57,27 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function disableDarkMode() {
+        var currentPage = window.location.pathname;
+        console.log(currentPage)
+        if (currentPage == '/index.html') {
+            console
+            document.getElementById("organization").classList.remove("section-main-dark-mode");
+            document.getElementById("partners").classList.remove("section-main-dark-mode");
+            document.getElementById("work_envolved").classList.remove("section-main-dark-mode");
 
-        /* BODY */
+
+        }
+        if (currentPage == '/register.html'){   
+            console.log('ingrese')
+            document.getElementById("main_register").classList.remove("section-main-dark-mode");
+        }
         document.body.classList.remove("body-dark-mode");
-        /* NAV  BACKGROUND */
+        document.getElementById("hero_metaverso").classList.remove("main-img-metaverso-dm");
         document.getElementById("nav_bar").classList.remove("bg-dark", "navbar-dark");
-
-        /* NAV LOGO */
         document.getElementById("nav_logo_light").classList.remove("d-none");
         document.getElementById("nav_logo_dark").classList.add("d-none")
-
-        /* SECTION AND MAIN */
-        document.getElementById("work_envolved").classList.remove("section-main-dark-mode");
-        document.getElementById("organization").classList.remove("section-main-dark-mode");
-        document.getElementById("partners").classList.remove("section-main-dark-mode");
-        document.getElementById("svg_1").classList.remove("section-main-dark-mode");
-        document.getElementById("svg_2").classList.remove("section-main-dark-mode");
-        document.getElementById("svg_3").classList.remove("section-main-dark-mode");
-        document.getElementById("svg_4").classList.remove("section-main-dark-mode");
-
         document.getElementById("hero_metaverso").classList.remove("main-img-metaverso-dm");
+
         svgs = document.getElementsByClassName("svg")
         Array.from(svgs).forEach(function (svg) {
             svg.classList.remove("waves-dm");
